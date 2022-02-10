886a4f87ce41ab0e4f72be8832f24eb16874bd62
Max Lin <mlin@suse.com>
2022-02-10 15:04:15 +0800

    up

---
+++ scripts/print_dead_pkgs.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+import argparse
+import logging
+import sys
+from urllib.error import HTTPError
+
+import re
+from xml.etree import cElementTree as ET
+
+import osc.conf
+import osc.core
+
+from osc import oscerr
+
+OPENSUSE = 'openSUSE:Leap:15.4'
+FACTORY = 'openSUSE:Factory'
+OPENSUSE_UPDATE = 'openSUSE:Leap:15.3:Update'
+BACKPORTS = 'openSUSE:Backports:SLE-15-SP4'
+SLE = 'SUSE:SLE-15-SP4:GA'
+
+makeurl = osc.core.makeurl
+http_GET = osc.core.http_GET
+http_POST = osc.core.http_POST
+http_PUT = osc.core.http_PUT
+
+class FindSLE(object):
+    def __init__(self, project, verbose):
+        self.project = project
+        self.verbose = verbose
+        self.apiurl = osc.conf.config['apiurl']
+        self.debug = osc.conf.config['debug']
+
+    def get_source_packages(self, project, expand=False):
+        """Return the list of packages in a project."""
+        query = {}
+        if expand:
+            query['expand'] = 1
+        root = ET.parse(http_GET(makeurl(self.apiurl, ['source', project],
+                                 query=query))).getroot()
+        packages = [i.get('name') for i in root.findall('entry')]
+
+        return packages
+
+    def item_exists(self, project, package=None):
+        """
+        Return true if the given project or package exists
+        """
+        if package:
+            url = makeurl(self.apiurl, ['source', project, package, '_meta'])
+        else:
+            url = makeurl(self.apiurl, ['source', project, '_meta'])
+        try:
+            http_GET(url)
+        except HTTPError as e:
+            if e.code == 404:
+                return False
+            else:
+                raise e
+        return True
+
+    def has_diff(self, project, package, target_prj, target_pkg):
+        changes_file = package + ".changes"
+        query = {'cmd': 'diff',
+                 'view': 'xml',
+                 'file': changes_file,
+                 'oproject': project,
+                 'opackage': package}
+        u = makeurl(self.apiurl, ['source', target_prj, target_pkg], query=query)
+        root = ET.parse(http_POST(u)).getroot()
+        if root:
+            # check if it has diff element
+            diffs = root.findall('files/file/diff')
+            if diffs:
+                return True
+        return False
+
+    def get_prj_results(self, prj, arch, code='failed'):
+        url = makeurl(self.apiurl, ['build', prj, "_result?arch=x86_64&repository=standard&view=status"])
+        results = []
+
+        root = ET.parse(http_GET(url)).getroot()
+
+        xmllines = root.findall("./result/status")
+
+        for pkg in xmllines:
+            if code == 'failed':
+                if pkg.attrib['code'] == 'failed' or pkg.attrib['code'] == 'unresolvable':
+                    if ':' not in pkg.attrib['package']:
+                        results.append(pkg.attrib['package'])
+            if code == 'succeeded':
+                if pkg.attrib['code'] == 'succeeded':
+                    if ':' not in pkg.attrib['package']:
+                        results.append(pkg.attrib['package'])
+
+        return results
+
+    def crawl(self):
+        """Main method"""
+        sle_pkglist = self.get_source_packages(SLE, True)
+        factory_pkglist = self.get_source_packages(FACTORY)
+        bp_pkglist = self.get_source_packages(BACKPORTS)
+        bp_fails = self.get_prj_results(BACKPORTS, 'x86_64', code='failed')
+        sle_ones = []
+        deletes = []
+        sle_fails = []
+
+        for pkg in bp_pkglist:
+            if pkg not in factory_pkglist:
+                if pkg in sle_pkglist:
+                    sle_ones.append(pkg)
+                else:
+                    deletes.append(pkg)
+
+        print("\nExists in SLE:")
+        for pkg in sle_ones:
+            print(pkg)
+
+        print("\nDeleted in Factory:")
+        for pkg in deletes:
+            print(pkg)
+
+        for pkg in bp_fails:
+            if pkg in sle_pkglist:
+                sle_fails.append(pkg)
+
+        print("\nSLE build failed in Backports:")
+        for pkg in sle_fails:
+            print(pkg)
+
+
+def main(args):
+    # Configure OSC
+    osc.conf.get_config(override_apiurl=args.apiurl)
+    osc.conf.config['debug'] = args.debug
+
+    uc = FindSLE(args.project, args.verbose)
+    uc.crawl()
+
+if __name__ == '__main__':
+    description = 'Compare packages status between two project'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-A', '--apiurl', metavar='URL', help='API URL')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='print info useful for debuging')
+    parser.add_argument('-p', '--project', dest='project', metavar='PROJECT',
+                        help='the target project where do submit to')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='show the diff')
+
+    args = parser.parse_args()
+
+    # Set logging configuration
+    logging.basicConfig(level=logging.DEBUG if args.debug
+                        else logging.INFO)
+
+    sys.exit(main(args))
---
+++ scripts/print_factory_new.py
@@ -0,0 +1,318 @@
+#!/usr/bin/python3
+
+import argparse
+import logging
+import sys
+import time
+
+from urllib.error import HTTPError, URLError
+
+import random
+import re
+from xml.etree import cElementTree as ET
+
+import osc.conf
+import osc.core
+from osclib.core import devel_project_get
+from osclib.core import project_pseudometa_package
+
+from osc import oscerr
+from osclib.memoize import memoize
+
+BACKPORTS = 'openSUSE:Backports:SLE-15-SP4'
+OPENSUSE = 'openSUSE:Leap:15.4'
+SLE = 'SUSE:SLE-15-SP4:GA'
+REBUILD_PROJECT = '{}:RebuildFactoryCandidates'.format(BACKPORTS)
+FACTORYFORK = '{}:FactoryFork'.format(BACKPORTS)
+SUPPORTED_ARCHS = ['x86_64', 'aarch64', 'ppc64le']
+FACTORY = 'openSUSE:Factory'
+
+makeurl = osc.core.makeurl
+http_GET = osc.core.http_GET
+http_POST = osc.core.http_POST
+http_PUT = osc.core.http_PUT
+
+class FccFreezer(object):
+    def __init__(self, freeze_rebuild):
+        self.apiurl = osc.conf.config['apiurl']
+        self.debug = osc.conf.config['debug']
+        self.freeze_rebuild = freeze_rebuild
+
+    def list_packages(self, project):
+        url = makeurl(self.apiurl, ['source', project])
+        pkglist = []
+
+        root = ET.parse(http_GET(url)).getroot()
+        xmllines = root.findall("./entry")
+        for pkg in xmllines:
+            pkglist.append(pkg.attrib['name'])
+
+        return set(pkglist)
+
+    def get_source_packages(self, project, expand=False):
+        """Return the list of packages in a project."""
+        query = {}
+        if expand:
+            query['expand'] = 1
+        root = ET.parse(http_GET(makeurl(self.apiurl, ['source', project],
+                                 query=query))).getroot()
+        packages = [i.get('name') for i in root.findall('entry')]
+
+        return packages
+
+    def check_one_source(self, flink, si, pkglist, os_pkglist, ignored_pkgs, sle_pkglist, bp_pkglist, factoryfork_pkglist):
+        """
+        Insert package information to the temporary frozenlinks.
+        Return package name if the package can not fit the condition
+        add to the frozenlinks, can be the ignored package.
+        """
+        package = si.get('package')
+        logging.debug("Processing %s" % (package))
+
+        # If the package is an internal one (e.g _product)
+        if package.startswith('_') or package.startswith('Test-DVD') or package.startswith('000') or package.startswith('preinstallimage'):
+            return None
+
+        if 'tumbleweed' in package:
+            return package
+
+        if package in ignored_pkgs:
+            return package
+
+        # filter out multibuild package
+        for originpackage in si.findall('originpackage'):
+            if ':' in package and package.split(':')[0] == originpackage.text:
+                return package
+
+        if self.freeze_rebuild:
+            if package in sle_pkglist or package in bp_pkglist or package in os_pkglist:
+                return package
+            if package in factoryfork_pkglist:
+                return package
+
+        for linked in si.findall('linked'):
+            if linked.get('project') == FACTORY:
+                url = makeurl(self.apiurl, ['source', FACTORY, package], {'view': 'info', 'nofilename': '1'})
+                f = http_GET(url)
+                proot = ET.parse(f).getroot()
+                lsrcmd5 = proot.get('lsrcmd5')
+                if lsrcmd5 is None:
+                    raise Exception("{}/{} is not a link but we expected one".format(FACTORY, package))
+                ET.SubElement(flink, 'package', {'name': package, 'srcmd5': lsrcmd5, 'vrev': si.get('vrev')})
+                return None
+
+        if package in ['rpmlint-mini-AGGR']:
+            # we should not freeze aggregates
+            return None
+
+        ET.SubElement(flink, 'package', {'name': package, 'srcmd5': si.get('srcmd5'), 'vrev': si.get('vrev')})
+        return None
+
+    def sources_to_ignore(self, flink):
+        ignored_sources = []
+        os_pkglist = self.get_source_packages(OPENSUSE, True)
+        pkglist = self.get_source_packages(FACTORY)
+        sle_pkglist = self.get_source_packages(SLE, True)
+        bp_pkglist = self.get_source_packages(BACKPORTS)
+        factoryfork_pkglist = self.get_source_packages(FACTORYFORK)
+
+        url = makeurl(self.apiurl, ['source', FACTORY], {'view': 'info', 'nofilename': '1'})
+        f = http_GET(url)
+        root = ET.parse(f).getroot()
+
+        ignored_pkgs = []
+        ignored_develprjs = ['KDE:Applications', 'KDE:Qt5', 'devel:languages:haskell', 'devel:kubic', 'KDE:Frameworks5', 'mozilla:Factory', 'KDE:Qt:5.15', 'devel:languages:ruby:extensions', 'security:SELinux', 'devel:languages:rust', 'science:HPC', 'devel:tools:building', 'server:php:extensions', 'devel:CaaSP', 'devel:CaaSP:Head:ControllerNode', 'system:install:head', 'mobile:synchronization:FACTORY', 'X11:Deepin', 'Java:Factory', 'devel:languages:javascript', 'devel:languages:ruby', 'Base:System', 'windows:mingw:win32', 'Java:packages', 'Virtualization:containers:images', 'X11:Pantheon', 'Virtualization:Appliances:Images:openSUSE-Tumbleweed', 'Application:ERP:GNUHealth:Factory', 'devel:languages:python:jupyter', 'devel:languages:python:azure', 'devel:languages:python:aws', 'devel:languages:python', 'Cloud:OpenStack:Factory', 'devel:languages:python:flask', 'devel:languages:python:avocado', 'devel:languages:python:django', 'devel:languages:python:aliyun', 'devel:languages:python:pytest', 'devel:languages:python:pyramid', 'server:monitoring', 'server:monitoring:zabbix','server:monitoring:thruk','server:monitoring:gearman', 'windows:mingw:win64', 'Application:Dochazka', 'devel:languages:python:numeric', 'science:machinelearning', 'Emulators', 'devel:openQA:tested', 'electronics', 'X11:Cinnamon:Factory', 'X11:MATE:Factory', 'X11:LXQt', 'Publishing:TeXLive', 'devel:languages:perl', 'devel:languages:ocaml', 'science', 'devel:languages:python:Factory', 'devel:languages:lua']
+
+        for prj in ignored_develprjs:
+            ignored_pkgs = ignored_pkgs + self.get_source_packages(prj)
+
+        pending_requests = [r.actions[0].tgt_package for r in self.get_request_list(BACKPORTS)]
+        ignored_pkgs = ignored_pkgs + pending_requests
+
+
+        for si in root.findall('sourceinfo'):
+            package = self.check_one_source(flink, si, pkglist, os_pkglist, ignored_pkgs, sle_pkglist, bp_pkglist, factoryfork_pkglist)
+            if package is not None:
+                ignored_sources.append(str(package))
+        return ignored_sources
+
+    def freeze(self, project):
+        """Main method"""
+        flink = ET.Element('frozenlinks')
+
+        fl = ET.SubElement(flink, 'frozenlink', {'project': FACTORY})
+        ignored_sources = self.sources_to_ignore(fl)
+        if self.debug:
+            logging.debug("Dump ignored source")
+            for source in ignored_sources:
+                logging.debug("Ignored source: %s" % source)
+
+        url = makeurl(self.apiurl, ['source', project, '_project', '_frozenlinks'], {'meta': '1'})
+        l = ET.tostring(flink)
+        # print(l)
+        try:
+            http_PUT(url, data=l)
+        except HTTPError as e:
+            raise e
+
+    def create_submitrequest(self, src_project, package, dst_project):
+        """Create a submit request using the osc.commandline.Osc class."""
+
+        msg = "Automatically create request by update submitter. This is trying to update package to %s from %s. Please review this change and decline it if Leap do not need it." % (dst_project, src_project)
+        res = osc.core.create_submit_request(self.apiurl,
+                                             src_project,
+                                             package,
+                                             dst_project,
+                                             package,
+                                             message=msg)
+        return res
+
+    def get_request_list(self, project):
+        return osc.core.get_request_list(self.apiurl, project, req_state=('new', 'review', 'declined', 'revoked'))
+
+    def check_multiple_specfiles(self, project, package):
+        try:
+            url = makeurl(self.apiurl, ['source', project, package], { 'expand': '1' } )
+        except HTTPError as e:
+            if e.code == 404:
+                return None
+            raise e
+        root = ET.fromstring(http_GET(url).read())
+        data = {}
+        linkinfo = root.find('linkinfo')
+        if linkinfo is not None:
+            data['linkinfo'] = linkinfo.attrib['package']
+        else:
+            data['linkinfo'] = None
+
+        files = [ entry.get('name').replace('.spec', '') for entry in root.findall('entry') if entry.get('name').endswith('.spec') ]
+        data['specs'] = files
+
+        if len(files) > 1:
+            return data
+        else:
+            return False
+
+    def get_build_succeeded_packages(self, project, arch):
+        """Get the build succeeded packages from `from_prj` project.
+        """
+        buildresult = osc.core.show_prj_results_meta(self.apiurl, REBUILD_PROJECT)
+        root = ET.fromstringlist(buildresult)
+        failed_multibuild_pacs = []
+        pacs = []
+        for node in root.findall('result'):
+            if node.get('repository') == 'standard' and node.get('arch') == arch:
+                for pacnode in node.findall('status'):
+                    if ':' in pacnode.get('package'):
+                        mainpac = pacnode.get('package').split(':')[0]
+                        if pacnode.get('code') not in ['succeeded', 'excluded']:
+                            failed_multibuild_pacs.append(pacnode.get('package'))
+                            if mainpac not in failed_multibuild_pacs:
+                                failed_multibuild_pacs.append(mainpac)
+                            if mainpac in pacs:
+                                pacs.remove(mainpac)
+                        else:
+                            if mainpac in failed_multibuild_pacs:
+                                failed_multibuild_pacs.append(pacnode.get('package'))
+                            elif mainpac not in pacs:
+                                pacs.append(mainpac)
+                        continue
+                    if pacnode.get('code') == 'succeeded':
+                        pacs.append(pacnode.get('package'))
+                    else:
+                        failed_multibuild_pacs.append(pacnode.get('package'))
+
+        return pacs
+
+    def list_pkgs(self):
+        """List build succeeded packages"""
+        for arch in SUPPORTED_ARCHS:
+            succeeded_packages = self.get_build_succeeded_packages(REBUILD_PROJECT, arch)
+            if not len(succeeded_packages) > 0:
+                logging.info('No build succeeded package in %s/%s' % (REBUILD_PROJECT,arch))
+                return
+
+            print('Build succeeded packages for %s:' % arch)
+            print('-------------------------------------')
+            for pkg in sorted(succeeded_packages):
+                print(pkg)
+
+            print('-------------------------------------')
+            print("Found {} build succeded packages for {}".format(len(succeeded_packages), arch))
+
+    def send_updates(self):
+        pending_requests = [r.actions[0].tgt_package for r in self.get_request_list(BACKPORTS)]
+        ms_packages = []
+        succeeded_packages = self.get_build_succeeded_packages(REBUILD_PROJECT, 'x86_64')
+        for package in sorted(succeeded_packages):
+            to_submit = True
+
+            multi_specs = self.check_multiple_specfiles(FACTORY, package)
+            if multi_specs is None:
+                logging.debug('%s does not exist in %s' % (package, 'openSUSE:Factory'))
+                to_submit = False
+
+            if multi_specs:
+                if multi_specs['linkinfo']:
+                    logging.debug('%s in %s is sub-package of %s, skip it!' % (package, 'openSUSE:Factory', multi_specs['linkinfo']))
+                    ms_packages.append(package)
+                    to_submit = False
+
+                for spec in multi_specs['specs']:
+                    if spec not in succeeded_packages:
+                        logging.debug('%s is sub-pacakge of %s but build failed, skip it!' % (spec, package))
+                        to_submit = False
+
+            if not to_submit:
+                continue
+
+            if package not in pending_requests:
+                res = self.create_submitrequest(FACTORY, package, BACKPORTS)
+                if res and res is not None:
+                    logging.info('Created request %s for %s' % (res, package))
+                else:
+                    logging.error('Error occurred when creating submit request')
+            else:
+                logging.info('%s is pending on %s, skip!' % (package, BACKPORTS))
+            time.sleep(5)
+
+        # dump multi specs packages
+        print("Multi-specfile packages:")
+        if len(ms_packages) > 0:
+            for pkg in ms_packages:
+                print(pkg)
+        else:
+            print('None')
+
+
+def main(args):
+    # Configure OSC
+    osc.conf.get_config(override_apiurl=args.apiurl)
+    osc.conf.config['debug'] = args.debug
+
+    freezer = FccFreezer(args.freeze_rebuild)
+    if args.list_packages:
+        freezer.list_pkgs()
+    elif args.submit:
+        freezer.send_updates()
+    elif args.freeze_rebuild:
+        print('freezing {}'.format(REBUILD_PROJECT))
+        freezer.freeze(REBUILD_PROJECT)
+
+if __name__ == '__main__':
+    description = 'Backports FactoryCandidates/SLECandidates freezer'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-A', '--apiurl', metavar='URL', help='API URL')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='print info useful for debuging')
+    parser.add_argument('-l', '--list', dest='list_packages', action='store_true', help='list build succeeded packages')
+    parser.add_argument('-f', '--freeze', dest='freeze_rebuild', action='store_true', help='update frozenlinks of RebuildFactoryCandidates')
+    parser.add_argument('-s', '--submit', dest='submit', action='store_true', help='submit updates from Factory to Backports')
+
+    args = parser.parse_args()
+
+    # Set logging configuration
+    logging.basicConfig(level=logging.DEBUG if args.debug
+                        else logging.INFO)
+
+    sys.exit(main(args))
