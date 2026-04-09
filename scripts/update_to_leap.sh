f40e48dad85db0f7e7b4fabefccf5879dc5b2940
Max Lin <mlin@suse.com>
2026-04-09 16:02:14 +0800

    ask username as the mandatory argument

--- scripts/update_to_leap.sh
+++ scripts/update_to_leap.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-        echo "Usage: $0 <package_name>"
+if [ "$#" -ne 2 ]; then
+        echo "Usage: $0 <username> <package_name>"
         exit 1
 fi
 
-package_name="$1"
+user_name="$1"
+package_name="$2"
 
 datestamp=$(date +"%Y%m%d")
 
 git-obs repo fork pool/$package_name
-git-obs repo clone mlin7442/$package_name
+git-obs repo clone $user_name/$package_name
 cd $package_name
 git checkout -b 161_branch_$datestamp origin/leap-16.1
 # git merge --no-commit factory
@@ -19,4 +20,4 @@ git merge --no-ff --allow-unrelated-histories -X theirs --no-commit factory
 git read-tree --reset -u factory
 git commit -m "Merge branch 'factory' into 'leap-16.1'"
 git push origin 161_branch_$datestamp
-git-obs pr create --title "Update to Factory version" --description "Update to Factory version" --source-owner mlin7442 --source-repo $package_name --source-branch 161_branch_$datestamp --target-owner pool --target-branch leap-16.1
+git-obs pr create --title "Update to Factory version" --description "Update to Factory version" --source-owner $user_name --source-repo $package_name --source-branch 161_branch_$datestamp --target-owner pool --target-branch leap-16.1
